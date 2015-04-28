51d905f2618f5f9b78d48233feddc874ac0d40de
Paco Jain <pacojain@gmail.com>
2015-04-28 17:08:14 -0500

    Added function stepTrace that sequentially steps through evaluation until result doesn't change (or "MaxSteps" iterations are reached).

--- VisualDebug.m
+++ VisualDebug.m
@@ -11,25 +11,38 @@ BeginPackage["VisualDebug`"]
 
 VisualDebug::usage= "Main utility"
 step
+stepTrace
 shape
 
 Begin["`Private`"]
 (* Implementation of the package *)
 
-SetAttributes[VisualDebug, HoldAll]
+SetAttributes[stepTrace, HoldFirst]
+(*ClearAttributes[stepTrace, HoldFirst]*)
+stepTrace[expr_, OptionsPattern[{"OutputWrapper" -> Defer, "HaltingContexts" -> {"Global`"}, "MaxSteps"-> 100}]] := Module[
+	{
+		currentExpr, newExpr = HoldForm[expr], stepNumber = 0
+	},
+	While[stepNumber < OptionValue["MaxSteps"] && ! MatchQ[newExpr, currentExpr],
+		stepNumber++;
+		currentExpr = newExpr;
+		newExpr = step @@ currentExpr;
+		Sow[currentExpr]
+	] // Reap // Part[#, 2, 1]&
+]
 
 SetAttributes[step, HoldFirst]
-(*ClearAttributes[step,HoldFirst]*)
-step[expr_, OptionsPattern[{"OutputWrapper" -> Defer, "HaltingContexts" -> {"Global`"}}]] := Module[
+(*ClearAttributes[step, HoldFirst]*)
+step[expr_, OptionsPattern[{"OutputWrapper" -> HoldForm, "HaltingContexts" -> {"Global`"}}]] := Module[
 	{
 		trace, tracePart, contexts, replacementTarget
 	},
 	trace = Trace[expr];
-	If[trace === {}, Return[expr]];
+	If[trace === {}, Return[expr // OptionValue["OutputWrapper"]]];
 	contexts[ex_] := Context /@ Cases[ex, s_Symbol -> HoldPattern[s], {0, Infinity}, Heads -> True] // DeleteDuplicates;
 	tracePart = ReplaceRepeated[trace, List[first_List, rest___] -> first];
 	replacementTarget = Select[tracePart // Rest, Intersection[contexts[#], OptionValue["HaltingContexts"]] != {} &] // If[# == {}, Last[tracePart], First[#]] &;
-	OptionValue["OutputWrapper"][expr] /. (First[tracePart] /. HoldForm -> HoldPattern) -> replacementTarget
+	OptionValue["OutputWrapper"][expr] /. (First[tracePart] /. HoldForm -> HoldPattern) :> RuleCondition @ Extract[replacementTarget, 1, $ConditionHold]
 ]
 
 SetAttributes[shape, HoldAll]
